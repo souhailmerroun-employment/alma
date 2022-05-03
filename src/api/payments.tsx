66b8fc337794a89d811ce408cf8380869924250e
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-05-03 20:04:58 +0200

    Code refactor

---
+++ src/api/payments.tsx
@@ -0,0 +1,18 @@
+type Merchant = {
+    name: string;
+}
+
+type Payment = {
+    status: string,
+    id: string,
+    created: Date,
+    customer_name: string,
+    merchant: Merchant
+    amount: number
+    installmentsCount: number
+}
+
+export const API_GET_PAYMENTS = async (): Promise<Payment[]> => {
+    const res = await fetch('https://cloudrun-frontend-recruitment-test-5hhyjiivra-ew.a.run.app/payments')
+    return res.json();
+}
--- src/pages/Payments.tsx
+++ src/pages/Payments.tsx
@@ -1,27 +1,9 @@
 import { useQuery } from "react-query";
-
-type Merchant = {
-    name: string;
-}
-
-type Payment = {
-    status: string,
-    id: string,
-    created: Date,
-    customer_name: string,
-    merchant: Merchant
-    amount: number
-    installmentsCount: number
-}
-
-const GET_PAYMENTS = async (): Promise<Payment[]> => {
-    const res = await fetch('https://cloudrun-frontend-recruitment-test-5hhyjiivra-ew.a.run.app/payments')
-    return res.json();
-}
+import { API_GET_PAYMENTS } from "../api/payments";
 
 export default function About() {
 
-    const { isLoading, data } = useQuery("payments", GET_PAYMENTS);
+    const { isLoading, data } = useQuery("payments", API_GET_PAYMENTS);
 
     console.log({ isLoading });
     console.log({ data });
