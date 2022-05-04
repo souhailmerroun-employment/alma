0f5a97fdf82322faac4058ce48dab0789b5d1627
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-05-04 12:33:51 +0200

    Enable globalFilter

--- src/components/PaymentsTable.tsx
+++ src/components/PaymentsTable.tsx
@@ -1,8 +1,9 @@
 import { Payment } from "../api/payments"
-import { useSortBy, useTable, useFilters } from "react-table";
+import { useSortBy, useTable, useFilters, useGlobalFilter } from "react-table";
 import { Link } from "react-router-dom";
 import { ROUTE_PAYMENTS_SHOW } from "../pages/routes";
 import { StatusFilter } from "./StatusFilter";
+import { GlobalFilter } from "./GlobalFilter";
 
 type ChildrenProps = {
     children: React.ReactNode
@@ -10,7 +11,7 @@ type ChildrenProps = {
 
 const Table = ({ children, ...props }: ChildrenProps) => <table className="min-w-full" {...props}>{children}</table>
 const THead = ({ children, ...props }: ChildrenProps) => <thead className="border-b" {...props}>{children}</thead>
-const TBody = ({ children, ...props }: ChildrenProps) => <thead {...props}>{children}</thead>
+const TBody = ({ children, ...props }: ChildrenProps) => <tbody {...props}>{children}</tbody>
 const TR = ({ children, ...props }: ChildrenProps) => <tr className="border-b" {...props}>{children}</tr>
 const TH = ({ children, ...props }: ChildrenProps) => <th className="text-sm font-medium text-gray-900 px-6 py-4 text-left" {...props}>{children}</th>
 const TD = ({ children, ...props }: ChildrenProps) => <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap" {...props}>{children}</td>
@@ -66,53 +67,60 @@ export default function PaymentsTable({ data }: Props) {
     const TableInstance = useTable({
         // @ts-ignore
         columns: columns,
-        data: data
-    }, useFilters, useSortBy)
+        data: data,
+    }, useGlobalFilter, useFilters, useSortBy)
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = TableInstance
+    // @ts-ignore
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, setGlobalFilter } = TableInstance
+
+    // @ts-ignore
+    const { globalFilter } = state;
 
     return (
-        <Table {...getTableProps()}>
-            <THead>
-                {headerGroups.map(headerGroup => (
-                    <TR {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            // @ts-ignore
-                            <TH {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                {column.render('Header')}
-                                <span>
-                                    {
-                                        // @ts-ignore
-                                        column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : null
-                                    }
-                                </span>
-                                <span>
-                                    {
-                                        // @ts-ignore
-                                        column.canFilter ? column.render('Filter') : null
-                                    }
-                                </span>
-                            </TH>
-                        ))}
-                    </TR>
-                ))}
-            </THead>
-            <TBody {...getTableBodyProps()}>
-                {rows.map((row, i) => {
-                    prepareRow(row)
-                    return (
-                        <TR {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                                return (
-                                    <TD {...cell.getCellProps()}>
-                                        {cell.render('Cell')}
-                                    </TD>
-                                )
-                            })}
+        <>
+            <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+            <Table {...getTableProps()}>
+                <THead>
+                    {headerGroups.map(headerGroup => (
+                        <TR {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                // @ts-ignore
+                                <TH {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                    {column.render('Header')}
+                                    <button>
+                                        {
+                                            // @ts-ignore
+                                            column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : null
+                                        }
+                                    </button>
+                                    <span>
+                                        {
+                                            // @ts-ignore
+                                            column.canFilter ? column.render('Filter') : null
+                                        }
+                                    </span>
+                                </TH>
+                            ))}
                         </TR>
-                    )
-                })}
-            </TBody>
-        </Table>
+                    ))}
+                </THead>
+                <TBody {...getTableBodyProps()}>
+                    {rows.map((row, i) => {
+                        prepareRow(row)
+                        return (
+                            <TR {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <TD {...cell.getCellProps()}>
+                                            {cell.render('Cell')}
+                                        </TD>
+                                    )
+                                })}
+                            </TR>
+                        )
+                    })}
+                </TBody>
+            </Table>
+        </>
     )
 }
